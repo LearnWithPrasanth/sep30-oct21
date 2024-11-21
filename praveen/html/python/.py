2671ac7d7aff346fe6747264ab080c52553f06c6
praveen <p9381895498@gamil.com>
2024-11-21 11:13:34 +0530

    file

---
+++ praveen/html/python/.py
@@ -0,0 +1,244 @@
+'''a=10
+b=2
+print(a)
+a+=b
+print(a)
+a-=b
+print(a)
+a*=b
+print(a)
+a/=b
+print(a)
+a//=b
+print(a)
+a%=b
+print(a)
+a**=b
+print(a)'''
+'''print("sunday",end="\t")
+print("monday",end="\t")
+print("thesday",end="\t")
+print("wednesday",end="\t")
+print("thesday",end="\t")
+print("friday",end="\t")
+print("saturday",end="\t")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   <form>
+      <label for="">Name</label>
+      <input type="text" name="name">
+         <br>
+         <label for="">Email</label>
+      <input type="text" name="email">
+         <br>
+         <label for="">mobile no</label>
+         <input type="text" name="mobile">
+            <br>
+            <label for="">gender</label>
+            <input type="radio" name="" id="">
+               <label for="">male</label>
+               <input type="radio" name="" id="">
+                  <label for="">female</label>
+                  <br>
+      <button>submit</button>
+         </form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="10">
+<thead>
+    <tr>
+        <th>Name</th>
+        <th>Jobs</th>
+        <th>Working Experience</th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+    <td rowspan="3">john </td>
+   <td>Software Engineer</td>
+   <td>Data Analyst</td
+    <td></td>
+</tr>
+<tr>
+    <td>row 2 cell 2</td>
+    <td>row 2 cell 3</td>
+    </tr>
+    <tr>
+        <td colspan="3">Row 3 cell 1</td>
+           </tr>
+               </tbody>
+       </table>
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               <input type="range"
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   <form>
+      <label for="">Name</label>
+      <input type="text" name="name" id="">
+         <br>
+         <label for="">Email</label>
+      <input type="text" name="email">
+         <br>
+         <label for="">mobile no</label>
+         <input type="text" name="mobile">
+            <br>
+            <label for="">gender</label>
+            <input type="radio" name="" id="">
+               <label for="">male</label>
+               <input type="radio" name="" id="">
+                  <label for="">female</label>
+                  <br>
+                 <input type="color">
+                  <br>
+                  <datalist id="state-list">
+                  <option value="andhra"></option>
+               </datalist>
+               <br>
+               <input type="hidden" value="secret">
+               <input type="range"
+      <button>submit</button>  
+         </form>
+</body>
+</html>
+
+readonly,
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   <form>
+      <form method="get">
+      <label for="name">Name</label
+      <input type="text" name="name" id="name">
+      <input type="text" name="name" id="name">
+         <br>
+         <label for="">Email</label>
+      <input type="email" name="email">
+         <br>
+         <label for="">mobile</label>
+         <input type="number" name="mobile">
+            <br>
+            <label for="">gender</label>
+            <br>
+            <input type="java">java</label>
+            <input type="checkbox" name="skills" id="sql" value="sql">
+            <label for="sql">sql</label>
+            <br>
+                  <label for="state">State</label>
+                  <select name="state" id="state">
+                     <option value="">select</option>
+                     <option value="ap">Andhra Pradesh</option>
+                     <option value="ts">Telangana</option>
+                     <option value="ks">karnataka</option>
+                  </select>
+                  <br>
+
+                  <label>state</label>
+                  <input name=""id="state" list="state-list">
+                  <datalist id="state-list">
+                     <option value="">select</option>
+                     <option value="ap">Andhra Pradesh</option>
+                     <option value="ts">Telangana</option>
+                     <option value="ks">karnataka</option>
+                     </datalist>
+                  <br>
+                  <button type="reset">Reset</button>
+         </form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
