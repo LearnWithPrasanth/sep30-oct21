a460bae6680d912e27763db077e740dd2344c45f
Prasanth Kumar Nagothi <nprasanth4046@gmail.com>
2024-12-04 15:40:28 +0530

    Javascript Basics

---
+++ javascript/script.js
@@ -0,0 +1,34 @@
+// Variables - const, let, var
+const fullName = "John Smith";
+let b = 20;
+var c = 30;
+// console.log(c);
+
+/*
+Datatypes
+1. Number
+2. String
+3. Boolean
+4. Undefined
+*/
+
+// let a;
+// console.log(typeof a);
+
+// let language = "Javascript";
+// console.log(`I love ${language}`);
+
+// Arithematic Operators --> +, -, *, /, %, **
+
+// Relational Operators --> <, >, <=, >=, ==, !=, ===, !==
+
+// Logical Operators --> &&, ||, !
+
+// let num = 0;
+// if (num > 0) {
+//     console.log("+Ve");
+// } else if (num < 0) {
+//     console.log("-Ve");
+// } else {
+//     console.log("Zero");
+// }
