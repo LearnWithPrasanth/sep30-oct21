2d3a834cb3ea91cdda3379dedd209be9b1e8b0eb
Prasanth Kumar Nagothi <nprasanth4046@gmail.com>
2024-12-05 15:30:38 +0530

    JavaScript Loops, Arrays, Objects

--- javascript/script.js
+++ javascript/script.js
@@ -32,3 +32,43 @@ Datatypes
 // } else {
 //     console.log("Zero");
 // }
+
+// Loops
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// let i = 1;
+// while (i <= 10) {
+//     if (i % 2 == 1) {
+//         console.log(i);
+//     }
+// }
+
+// Arrays
+let arr = [1, 2, "three", true, 5.0];
+// arr.push("new");
+// arr.pop();
+// b = [...arr];
+// b[1] = 10;
+console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let element of arr) {
+//     console.log(element);
+// }
+
+// Objects
+let employee = {
+    name: "John",
+    designation: "SDE I",
+    salary: "600000",
+};
+employee.name = "Smith";
+let keys = Object.keys(employee);
+console.log(keys);
