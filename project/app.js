d93054238ba598603403a95623fcbb279d9cbb89
Prasanth Kumar Nagothi <nprasanth4046@gmail.com>
2024-12-12 11:55:10 +0530

    Complete Todo Project - LocalStorage,

--- project/app.js
+++ project/app.js
@@ -15,6 +15,7 @@ let editID;
 // ********** EVENT LISTENERS **********
 form.addEventListener("submit", addTodo);
 clearBtn.addEventListener("click", clearTodos);
+window.addEventListener("DOMContentLoaded", loadTodos);
 
 // ********** FUNCTIONS **********
 function addTodo(e) {
@@ -30,6 +31,7 @@ function addTodo(e) {
     if (editFlag) {
         editElement.textContent = value;
         displayAlert("success", "Todo Edited");
+        editFromLocalStorage(editID, value);
         resetToDefault();
         return;
     }
@@ -62,6 +64,7 @@ function addTodo(e) {
     list.appendChild(element);
     container.classList.add("show-container");
     displayAlert("success", "Todo Added");
+    addToLocalStorage(id, value);
     resetToDefault();
 }
 
@@ -71,15 +74,17 @@ function editTodo(e) {
     submitBtn.textContent = "edit";
     editFlag = true;
     console.log(editElement);
+    let element = e.currentTarget.parentElement.parentElement;
+    editID = element.dataset.id;
 }
 
 function deleteTodo(e) {
     let element = e.currentTarget.parentElement.parentElement;
-
     list.removeChild(element);
     if (list.children.length === 0)
         container.classList.remove("show-container");
     displayAlert("danger", "todo deleted");
+    deleteFromLocalStorage(element.dataset.id);
 }
 
 function clearTodos() {
@@ -89,6 +94,7 @@ function clearTodos() {
     }
     container.classList.remove("show-container");
     displayAlert("danger", "all todos deleted");
+    localStorage.removeItem("todos");
 }
 
 function displayAlert(classname, message) {
@@ -108,5 +114,74 @@ function resetToDefault() {
 }
 
 // ********** LOCAL STORAGE **********
+function getFromLocalStorage() {
+    let todos = localStorage.getItem("todos");
+    return todos ? JSON.parse(todos) : [];
+}
+
+function addToLocalStorage(id, value) {
+    let todos = getFromLocalStorage();
+    let newTodo = { id, value }; // same as {id: id, value: value}
+    todos.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function deleteFromLocalStorage(id) {
+    let todos = getFromLocalStorage();
+    let updatedTodos = todos.filter((todo) => {
+        if (todo.id !== id) {
+            return todo;
+        }
+    });
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+}
+
+function editFromLocalStorage(id, value) {
+    let todos = getFromLocalStorage();
+    let updatedTodos = todos.map((todo) => {
+        if (todo.id === id) {
+            todo.value = value;
+        }
+        return todo;
+    });
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+}
 
 // ********** SETUP ITEMS **********
+function loadTodos() {
+    let todos = getFromLocalStorage();
+    for (const todo of todos) {
+        loadSingleTodo(todo.id, todo.value);
+    }
+
+    if (list.children.length > 0) container.classList.add("show-container");
+}
+
+function loadSingleTodo(id, value) {
+    let element = document.createElement("article");
+    let attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.classList.add("todo-item");
+    element.setAttributeNode(attr);
+
+    element.innerHTML = `
+        <p class="title">${value}</p>
+        <div class="btn-container">
+            <!-- edit btn -->
+            <button type="button" class="edit-btn">
+            <i class="fas fa-edit"></i>
+            </button>
+            <!-- delete btn -->
+            <button type="button" class="delete-btn">
+            <i class="fas fa-trash"></i>
+            </button>
+        </div>
+    `;
+
+    const editBtn = element.querySelector(".edit-btn");
+    editBtn.addEventListener("click", editTodo);
+    const deleteBtn = element.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", deleteTodo);
+
+    list.appendChild(element);
+}
